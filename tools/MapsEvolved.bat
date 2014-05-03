efe0abe615557996452646d33b106d0c1ed7c84f
Christian Aichinger <Greek0@gmx.net>
2014-05-03 19:07:43 +0200

    Improve output of tools/MapsEvolved.bat

--- tools/MapsEvolved.bat
+++ tools/MapsEvolved.bat
@@ -1,9 +1,13 @@
+@ECHO OFF
 pushd "%~dp0\.."
 
 taskkill /FI "WINDOWTITLE eq Maps Evolved" /FI "IMAGENAME eq python.exe"
 
 call venv\scripts\activate.bat
+
+@ECHO ON
 python MapsEvolved.py
+@ECHO OFF
 
 choice /T 6 /C kq /N /D q /M "Press 'K' to keep the console open:"
 IF %ERRORLEVEL%==1 pause
