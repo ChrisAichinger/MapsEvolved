335bf9e5c3b788f727befd30e6407bcabfa4f68d
Christian Aichinger <Greek0@gmx.net>
2014-05-20 00:15:14 +0200

    Remove more dead code
    Slim down winwrap.cpp and remove most of the platform-specific code.

--- OutdoorMapper/odm_config.h
+++ OutdoorMapper/odm_config.h
@@ -1,16 +1,14 @@
 #ifndef ODM__CONFIG_H
 #define ODM__CONFIG_H
 
-#define ODM_INTERFACE __declspec(novtable)
-
-#define ODM_OS WINDOWS
-
 
 #ifdef _MSC_VER
+
+#define ODM_OS WINDOWS
+#define ODM_INTERFACE __declspec(novtable)
 #define PACKED_STRUCT(...) __pragma(pack(push, 1)); \
                            __VA_ARGS__;             \
                            __pragma(pack(pop));
-
 #ifdef ODM_SHARED_LIB
 #  define EXPORT __declspec(dllexport)
 #elif ODM_EXECUTABLE
@@ -18,7 +16,6 @@
 #else
 #  define EXPORT __declspec(dllimport)
 #endif
-
 #pragma warning( disable : 4251 )
 
 #elif defined(__GNUC__)
