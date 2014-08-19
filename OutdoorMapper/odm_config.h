cc71a26ea9609f6125d72d552358827f89fd1c49
Christian Aichinger <Greek0@gmx.net>
2014-08-19 14:36:36 +0200

    Comment on suppressing compiler warning 4251

--- OutdoorMapper/odm_config.h
+++ OutdoorMapper/odm_config.h
@@ -16,6 +16,13 @@
 #else
 #  define EXPORT __declspec(dllimport)
 #endif
+
+// Disable warning about exporting STL classes as part of the DLL interface.
+// Cf. http://stackoverflow.com/a/5664491
+// The gist is: if we compile all our code with the same compiler version and
+// options, we can ignore this. We want to do that anyway for other reasons
+// (see http://www.virtualdub.org/blog/pivot/entry.php?id=296), so just go
+// ahead and suppress the warning.
 #pragma warning( disable : 4251 )
 
 #elif defined(__GNUC__)
