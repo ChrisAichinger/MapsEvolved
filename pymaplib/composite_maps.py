3e43995c235750eec7a5559bec805e4644a5ec5d
Christian Aichinger <Greek0@gmx.net>
2014-09-04 22:17:33 +0200

    Pymaplib: Clean up composite_maps.py
    Refactor the code to be more correct and more readable.

--- pymaplib/composite_maps.py
+++ pymaplib/composite_maps.py
@@ -18,99 +18,113 @@ class CombinableTile:
         self.map = container.drawable
         self.size = size = self.map.GetSize()
 
-        # Our neighbors: left, right, top, bottom.
-        self.l = self.r = self.t = self.b = None
+        self.neighbors = {side: None for side in 'lrtb'}
 
-        # The LatLon coordinates of our 4 corners.
-        ok1, self.tl = self.map.PixelToLatLon(MapPixelCoord(0, 0))
-        ok2, self.tr = self.map.PixelToLatLon(MapPixelCoord(size.x, 0))
-        ok3, self.bl = self.map.PixelToLatLon(MapPixelCoord(0, size.y))
-        ok4, self.br = self.map.PixelToLatLon(MapPixelCoord(size.x, size.y))
-        if not (ok1 and ok2 and ok3 and ok4):
-            raise RuntimeError("Failed to get map coordinates")
+        corners = {"tl": MapPixelCoord(0, 0),
+                   "tr": MapPixelCoord(size.x, 0),
+                   "bl": MapPixelCoord(0, size.y),
+                   "br": MapPixelCoord(size.x, size.y),
+                   }
+        self.latlon = dict()
+        self.mpp = dict()
+        for name, coord in corners.items():
+            ok, self.latlon[name] = self.map.PixelToLatLon(coord)
+            if not ok:
+                raise RuntimeError("Failed to get map corner coordinates")
+            ok, self.mpp[name] = maplib_sip.MetersPerPixel(
+                                       container.drawable, coord)
+            if not ok:
+                raise RuntimeError("Failed to determine meters/pixel for map")
 
         # Roughly estimate degrees/pixel to get an epsilon for later
         # coordinate equality checking.
-        size_lat = self.tl.lat - self.tr.lat
-        size_lon = self.tl.lon - self.tr.lon
-        degrees = math.sqrt(size_lat**2 + size_lon**2)
-        self.deg_per_pix = degrees / size.x
+        size_lat = self.latlon["tl"].lat - self.latlon["br"].lat
+        size_lon = self.latlon["tl"].lon - self.latlon["br"].lon
+        diagonal_deg = math.sqrt(size_lat**2 + size_lon**2)
+        diagonal_pix = math.sqrt(corners["br"].x**2 + corners["br"].y**2)
+        self.deg_per_pix = diagonal_deg / diagonal_pix
 
-        call_ok, self.mpp = maplib_sip.MetersPerPixel(container.drawable,
-                                                      MapPixelCoordInt(0, 0))
-        if not call_ok:
-            raise RuntimeError("Failed to determine meters/pixel for map")
-
-    def equal(self, latlon1, latlon2):
+    def ll_equal(self, latlon1, latlon2):
         """Check if two LatLon coordinates are equal
 
-        We consider them equal if their distance is less than (roughly) one
+        We consider them equal if their distance is less than (roughly) half a
         map pixel.
         """
-        return (abs(latlon1.lat - latlon2.lat) < self.deg_per_pix and
-                abs(latlon1.lon - latlon2.lon) < self.deg_per_pix)
+        return (abs(latlon1.lat - latlon2.lat) < 0.5 * self.deg_per_pix and
+                abs(latlon1.lon - latlon2.lon) < 0.5 * self.deg_per_pix)
 
-    def do_pair_with(self, other, self_direction):
-        """Pair two maps by making them neighbors of each other
+    def mpp_equal(self, mpp1, mpp2):
+        """Check if two meters/pixel values are equal
 
-        Make ``other`` a neighbor of ``self`` to the side ``self_direction``.
-        Also update ``other`` so that ``self`` is its neighbor in the opposite
-        direction.
+        We consider them equal, if they are within 1% of each other.
         """
 
-        if self_direction == 'l':
-            if self.l or other.r:
-                return
-            self.l = other
-            other.r = self
-        elif self_direction == 'r':
-            if self.r or other.l:
-                return
-            self.r = other
-            other.l = self
-        elif self_direction == 't':
-            if self.t or other.b:
-                return
-            self.t = other
-            other.b = self
-        elif self_direction == 'b':
-            if self.b or other.t:
-                return
-            self.b = other
-            other.t = self
+        average_mpp = (mpp1 + mpp2) / 2
+        mpp_delta = (mpp1 - mpp2) / average_mpp
+        return abs(mpp_delta) <= 0.01
+
+    def is_side_compatible(self, other, side):
+        """Check if this map can be paired with another one on a given side
+
+        Ensure the corner coordinates are close enough in the LatLon system,
+        that the resolution on the corners is similar, and that the maps
+        have the same number of pixels along the relevant side.
+
+        The ``side`` argument is seen from self, e.g. side='l' means the left
+        side of self will be paired with the right side of other.
+        """
+
+        corner_pairs = {
+                # Sides and what corners have to contact for the pair
+                # to match.
+                'l': [('tl', 'tr'), ('bl', 'br')],
+                'r': [('tr', 'tl'), ('br', 'bl')],
+                't': [('tl', 'bl'), ('tr', 'br')],
+                'b': [('bl', 'tl'), ('br', 'tr')],
+                }
+        (A_self, A_other), (B_self, B_other) = corner_pairs[side]
+        if not self.ll_equal(self.latlon[A_self], other.latlon[A_other]):
+            return False
+        if not self.mpp_equal(self.mpp[A_self], other.mpp[A_other]):
+            return False
+
+        if not self.ll_equal(self.latlon[B_self], other.latlon[B_other]):
+            return False
+        if not self.mpp_equal(self.mpp[B_self], other.mpp[B_other]):
+            return False
+
+        if side in 'lr':
+            return self.size.y == other.size.y
+        else:
+            return self.size.x == other.size.x
+
+    def do_pair_with(self, other, side_self):
+        """Pair two maps by making them neighbors of each other
+
+        Make ``other`` a neighbor of ``self`` to the side ``side_self``.
+        Also update ``other`` so that ``self`` is its neighbor on the opposite
+        side.
+        """
+
+        side_other = dict(zip('lrtb', 'rlbt'))[side_self]
+        if self.neighbors[side_self] or other.neighbors[side_other]:
+            return
+        self.neighbors[side_self] = other
+        other.neighbors[side_other] = self
 
     def try_pair_with(self, other):
         """Try to make ``other`` a neighbor of this object
 
-        Test if the maps have compatible scale (meters per pixel), then test
-        if they can be paired along any of their four sides. This is the case
-        if their corner coordinates (LatLon) along that side are equal.
-        Finally, ensure they have the same number of pixels along the side
-        where they are to be joined.
-
-        If they are pairable, pair them using do_pair_with().
+        Try to combine the two maps along each of their four sides.
+        If one of them fits, mark them as neighbors along that side.
 
         This operational is symmetrical regarding self and other:
             self.try_pair_with(other) is the same as other.try_pair_with(self)
         """
 
-        # Only merge maps if their MPP are within 1% of each other.
-        mpp_delta = (self.mpp - other.mpp) / self.mpp
-        if abs(mpp_delta) > 0.01:
-            return
-
-        if self.equal(self.tl, other.tr) and self.equal(self.bl, other.br):
-            if self.size.y == other.size.y:
-                self.do_pair_with(other, 'l')
-        elif self.equal(self.tr, other.tl) and self.equal(self.br, other.bl):
-            if self.size.y == other.size.y:
-                self.do_pair_with(other, 'r')
-        elif self.equal(self.tl, other.bl) and self.equal(self.tr, other.br):
-            if self.size.x == other.size.x:
-                self.do_pair_with(other, 't')
-        elif self.equal(self.bl, other.tl) and self.equal(self.br, other.tr):
-            if self.size.x == other.size.x:
-                self.do_pair_with(other, 'b')
+        for side in 'lrtb':
+            if self.is_side_compatible(other, side):
+                self.do_pair_with(other, side)
 
     def iter_right(self):
         """Iterate over the neighbors to the right (including self)"""
@@ -118,7 +132,7 @@ class CombinableTile:
         map = self
         while map:
             yield map
-            map = map.r
+            map = map.neighbors['r']
 
     def iter_down(self):
         """Iterate over the neighbors below (including self)"""
@@ -126,12 +140,13 @@ class CombinableTile:
         map = self
         while map:
             yield map
-            map = map.b
+            map = map.neighbors['b']
+
 
 class CompositableTiles:
     """Collection of tiles eligable for creating a CompositeMap
 
-    To create CompositableTiles, use the find_composities() function.
+    To create CompositableTiles, use the from_topleft_tile() classmethod.
     """
     def __init__(self, raster):
         self.num_x = len(raster)
@@ -149,6 +164,59 @@ class CompositableTiles:
         return maplib_sip.CompositeMap.FormatFname(
                 self.num_x, self.num_y, self.tile_maps)
 
+    @classmethod
+    def from_tile_graph(cls, topleft_tile):
+        """Create an instance from the top-left tile of a tile graph
+
+        Take the top-left tile of a tile graph, place the nodes into a
+        rectangular 2D array. Then, create a CompositableTiles object from
+        the array.
+
+        Return the created CompositableTiles object or None if the tile graph
+        was malformed, for example if it didn't form a rectangle.
+
+        Example input (tile1 is topleft_tile):
+            tile1.neighbors['r'] = tile2, tile1.neighbors['b'] = tile3
+            tile2.neighbors['l'] = tile1, tile2.neighbors['b'] = tile4
+            tile3.neighbors['r'] = tile4, tile3.neighbors['t'] = tile1
+            tile4.neighbors['l'] = tile3, tile4.neighbors['t'] = tile2
+        Visualization:
+            tile1 <-> tile2
+              |         |
+            tile3 <-> tile4
+        Rasterized result:
+            rester[0][0] = tile1, raster[1][0] = tile2
+            rester[0][1] = tile3, raster[1][1] = tile4
+        """
+
+        raster = [list(t for t in left_border_tile.iter_down())
+                  for left_border_tile in topleft_tile.iter_right()]
+
+        # Ensure all sublists have the same length.
+        if len(set(len(l) for l in raster)) != 1:
+            return None
+
+        # Number of elements in raster when indexed as raster[i][j]
+        # i increases from left to right, j from top to bottom.
+        num_tiles_x = len(raster)
+        num_tiles_y = len(raster[0])
+        for x in range(num_tiles_x):
+            for y in range(num_tiles_y):
+                try:
+                    expected = {
+                          "l": None if x == 0             else raster[x-1][y],
+                          "r": None if x == num_tiles_x-1 else raster[x+1][y],
+                          "t": None if y == 0             else raster[x][y-1],
+                          "b": None if y == num_tiles_y-1 else raster[x][y+1],
+                        }
+                    for side in 'lrtb':
+                        if raster[x][y].neighbors[side] != expected[side]:
+                            return None
+                except IndexError:
+                    return None
+        return cls(raster)
+
+
 def find_composites(maplist):
     """A generator producing CompositableTiles objects from a maplist"""
 
@@ -164,71 +232,20 @@ def find_composites(maplist):
 
     for containers in buckets.values():
         if len(containers) < 2:
-            # Can't create a composite of one single map.
             continue
 
+        # Iterate over all pairs of tiles and link the fitting tiles together.
         tiles = [CombinableTile(c) for c in containers]
         for tile1, tile2 in itertools.combinations(tiles, 2):
-            # Iterate over all pairs of tiles.
             tile1.try_pair_with(tile2)
 
-        # Find the topleft tiles.
-        tl_tiles = [tile for tile in tiles
-                    if tile.b and tile.r and not tile.l and not tile.t]
+        for tile in tiles:
+            # Find the top-left tiles and place them into a 2D raster.
+            if not (tile.neighbors['b'] or tile.neighbors['r']):
+                continue
+            if tile.neighbors['l'] or tile.neighbors['t']:
+                continue
 
-        # Each topleft tile is converted to a 2D array of tiles.
-        rasters = (_raster_from_topleft_tile(tl_tile) for tl_tile in tl_tiles)
-        for raster in rasters:
-            # Discard failed tile-to-raster conversions.
-            if raster:
-                yield CompositableTiles(raster)
-
-def _raster_from_topleft_tile(tl_tile):
-    """Take a top-left tile and produce a rectangular 2D array of tiles
-
-    Start from ``tl_tile`` and trace the graph generated by the neighbor
-    links (l/r/t/b). Create a rectangular 2D list-of-lists of the nodes.
-
-    The result may be None if the tile graph is malformed, for example if
-    it doesn't form a rectangle.
-
-    Example result:
-        [[tile1, tile2],
-         [tile3, tile4]]
-    """
-
-    num_tiles_x = len(list(tl_tile.iter_right()))
-    num_tiles_y = len(list(tl_tile.iter_down()))
-
-    raster = [[None] * num_tiles_y for i in range(num_tiles_x)]
-    try:
-        # Populate the tile raster down-right.
-        for k, leftborder_tile in enumerate(tl_tile.iter_down()):
-            if leftborder_tile.l:
-                # If we have an "outgrowing" tile to the left, abort.
-                return None
-            for j, tile in enumerate(leftborder_tile.iter_right()):
-                raster[j][k] = tile
-        # Ensure it is the same when viewed right-down (consistency check).
-        for j, topborder_tile in enumerate(tl_tile.iter_right()):
-            if topborder_tile.t:
-                # If we have an "outgrowing" tile to the top, abort.
-                return None
-            for k, tile in enumerate(topborder_tile.iter_down()):
-                if raster[j][k] != tile:
-                    return None
-        # We don't need to consistency-check up-left or left-up, as we
-        # treat l/r and t/b symmetrically in CombinableTile.
-
-    except IndexError:
-        # If j >= num_tiles_x or k >= num_tiles_y we land here.
-        # This means the raster is not rectangular, so abort.
-        return None
-
-    # If any None entries are left in the raster, abort as well.
-    # This complements the IndexError check above.
-    for row in raster:
-        if not all(row):
-            return None
-
-    return raster
+            ctiles = CompositableTiles.from_tile_graph(tile)
+            if ctiles:
+                yield ctiles
