41b7438fff7c690ec64b3297333aae2307f93588
Christian Aichinger <Greek0@gmx.net>
2014-09-05 00:59:05 +0200

    Support composite maps where tiles overlap
    Some maps, such as SRTM tiles and other global DHM datasets have one pixel
    overlap between adjacent tiles. Handle these cases correctly.

--- OutdoorMapper/map_composite.cpp
+++ OutdoorMapper/map_composite.cpp
@@ -9,18 +9,24 @@
 CompositeMap::CompositeMap(
        unsigned int num_x,
        unsigned int num_y,
+       bool has_overlap_pixel,
        const std::vector<std::shared_ptr<RasterMap> > &submaps)
-    : m_num_x(num_x), m_num_y(num_y), m_submaps(submaps),
-      m_width(0), m_height(0), m_fname(), m_title(), m_description()
+    : m_num_x(num_x), m_num_y(num_y),
+      m_overlap_pixel(has_overlap_pixel ? 1 : 0),
+      m_submaps(submaps), m_width(0), m_height(0), m_fname(), m_title(),
+      m_description()
 {
     init();
 }
 
 CompositeMap::CompositeMap(const std::wstring &fname_token)
-    : m_num_x(0), m_num_y(0), m_submaps(),
+    : m_num_x(0), m_num_y(0), m_overlap_pixel(0), m_submaps(),
       m_width(0), m_height(0), m_fname(), m_title(), m_description()
 {
-    m_submaps = LoadFnameMaps(fname_token, &m_num_x, &m_num_y);
+    bool has_overlap_pixel;
+    m_submaps = LoadFnameMaps(fname_token, &m_num_x, &m_num_y,
+                              &has_overlap_pixel);
+    m_overlap_pixel = (has_overlap_pixel ? 1 : 0);
     init();
 }
 
@@ -32,10 +38,10 @@ void CompositeMap::init() {
         throw std::runtime_error("Can not generate empty combined map");
     }
     for (unsigned int x = 0; x < m_num_x; ++x) {
-        m_width += m_submaps[index(x, 0)]->GetWidth();
+        m_width += SubmapWidth(x, 0);
     }
     for (unsigned int y = 0; y < m_num_y; ++y) {
-        m_height += m_submaps[index(0, y)]->GetHeight();
+        m_height += SubmapHeight(0, y);
     }
     m_fname = FormatFname(*this);
     m_title = L"Composite map";
@@ -61,20 +67,29 @@ CompositeMap::find_submap(T coord) const {
     // coord.x > submap.GetWidth(). Same consideration for the y axis.
     unsigned int x, y;
     for (x = 0; x < m_num_x - 1; ++x) {
-        unsigned int map_width = m_submaps[index(x, 0)]->GetWidth();
+        unsigned int map_width = SubmapWidth(x, 0);
         if (coord.x < static_cast<int>(map_width)) {
             break;
         }
         coord.x -= map_width;
     }
     for (y = 0; y < m_num_x - 1; ++y) {
-        unsigned int map_height = m_submaps[index(0, y)]->GetHeight();
+        unsigned int map_height = SubmapHeight(0, y);
         if (coord.y < static_cast<int>(map_height)) {
             break;
         }
         coord.y -= map_height;
     }
     return std::make_tuple(x, y, coord);
+}
+
+unsigned int
+CompositeMap::SubmapWidth(unsigned int mx, unsigned int my) const {
+    return m_submaps[index(mx, my)]->GetWidth() - m_overlap_pixel;
+}
+unsigned int
+CompositeMap::SubmapHeight(unsigned int mx, unsigned int my) const {
+    return m_submaps[index(mx, my)]->GetHeight() - m_overlap_pixel;
 }
 
 GeoDrawable::DrawableType CompositeMap::GetType() const {
@@ -107,11 +122,11 @@ CompositeMap::LatLonToPixel(const LatLon &pos, MapPixelCoord *output) const {
     MapPixelCoord res;
     unsigned int x_offset = 0;
     for (unsigned int x = 0; x < m_num_x; ++x) {
-        unsigned int cur_map_width = m_submaps[index(x, 0)]->GetWidth();
+        unsigned int cur_map_width = SubmapWidth(x, 0);
         unsigned int y_offset = 0;
         for (unsigned int y = 0; y < m_num_y; ++y) {
             auto cur_map = m_submaps[index(x, y)];
-            unsigned int cur_map_height = cur_map->GetHeight();
+            unsigned int cur_map_height = SubmapHeight(x, y);
             if (!cur_map->LatLonToPixel(pos, &res)) {
                 return false;
             }
@@ -180,14 +195,15 @@ PixelBuf CompositeMap::GetRegion(
 
             int x_start = (x == tl_x) ? tl_pos.x : 0;
             int y_start = (y == tl_y) ? tl_pos.y : 0;
-            int x_end =   (x == br_x) ? br_pos.x : map->GetWidth();
-            int y_end =   (y == br_y) ? br_pos.y : map->GetHeight();
+            int x_end =   (x == br_x) ? br_pos.x : SubmapWidth(x, y);
+            int y_end =   (y == br_y) ? br_pos.y : SubmapHeight(x, y);
 
             auto subregion_pos = MapPixelCoordInt(x_start, y_start);
             subregion_sz = MapPixelDeltaInt(x_end - x_start, y_end - y_start);
             PixelBuf subregion = map->GetRegion(subregion_pos, subregion_sz);
             // BitBlt subregion into our result region at [xy]_offset.
-            result.Insert(PixelBufCoord(x_offset, y_offset), subregion);
+            PixelBufCoord target(x_offset, size.y - subregion_sz.y - y_offset);
+            result.Insert(target, subregion);
 
             y_offset += subregion_sz.y;
         }
@@ -201,14 +217,16 @@ typedef std::regex_iterator<std::wstring::iterator> wstring_regex_it;
 std::vector<std::wstring>
 CompositeMap::ParseFname(const std::wstring &fname,
                          unsigned int *num_maps_x,
-                         unsigned int *num_maps_y)
+                         unsigned int *num_maps_y,
+                         bool *has_overlap_pixel)
 {
-    std::wregex re(L"composite_map:(\\d+);(\\d+);(.*)");
+    std::wregex re(L"composite_map:(\\d+);(\\d+);(clip|noclip);(.*)");
     std::wsmatch m;
     std::regex_match(fname, m, re);
     *num_maps_x = std::stoul(m[1]);
     *num_maps_y = std::stoul(m[2]);
-    std::wstring fnames_str(m[3]);
+    *has_overlap_pixel = (m[3] == L"clip");
+    std::wstring fnames_str(m[4]);
 
     std::vector<std::wstring> fnames;
     re = L"([^;]+)(?:;|$)";
@@ -223,10 +241,12 @@ CompositeMap::ParseFname(const std::wstring &fname,
 std::wstring
 CompositeMap::FormatFname(unsigned int num_maps_x,
                           unsigned int num_maps_y,
+                          bool has_overlap_pixel,
                           const std::vector<std::wstring> &fnames)
 {
     std::wostringstream ss;
     ss << L"composite_map:" << num_maps_x << L";" << num_maps_y << L";";
+    ss << (has_overlap_pixel ? L"clip" : L"noclip") << L";";
     for (auto it = fnames.cbegin(); it != fnames.cend(); ++it) {
         ss << url_encode(*it) << L";";
     }
@@ -236,9 +256,10 @@ CompositeMap::FormatFname(unsigned int num_maps_x,
 std::vector<std::shared_ptr<RasterMap>>
 CompositeMap::LoadFnameMaps(const std::wstring &fname,
                             unsigned int *num_maps_x,
-                            unsigned int *num_maps_y)
+                            unsigned int *num_maps_y,
+                            bool *has_overlap_pixel)
 {
-    auto fnames = ParseFname(fname, num_maps_x, num_maps_y);
+    auto fnames = ParseFname(fname, num_maps_x, num_maps_y, has_overlap_pixel);
     std::vector<std::shared_ptr<RasterMap>> maps;
     for (auto it = fnames.cbegin(); it != fnames.cend(); ++it) {
         maps.push_back(LoadMap(*it));
@@ -249,16 +270,17 @@ CompositeMap::LoadFnameMaps(const std::wstring &fname,
 std::wstring
 CompositeMap::FormatFname(
     unsigned int num_maps_x, unsigned int num_maps_y,
+    bool has_overlap_pixel,
     const std::vector<std::shared_ptr<RasterMap>> &submaps)
 {
     std::vector<std::wstring> fnames;
     for (auto it = submaps.cbegin(); it != submaps.cend(); ++it) {
         fnames.push_back((*it)->GetFname());
     }
-    return FormatFname(num_maps_x, num_maps_y, fnames);
+    return FormatFname(num_maps_x, num_maps_y, has_overlap_pixel, fnames);
 }
 
 std::wstring CompositeMap::FormatFname(const CompositeMap &this_map) {
     return FormatFname(this_map.m_num_x, this_map.m_num_y,
-                       this_map.m_submaps);
+                       this_map.m_overlap_pixel != 0, this_map.m_submaps);
 }
--- OutdoorMapper/map_composite.h
+++ OutdoorMapper/map_composite.h
@@ -9,9 +9,40 @@
 #include "coordinates.h"
 #include "rastermap.h"
 
+/* Present multiple RasterMaps collectively as one map. The individual maps
+ * are treated like tiles and placed directly next to each other.
+ *
+ * The mystery of has_overlap_pixel (aka m_overlap_pixel):
+ * Map tiles come in two variants:
+ * 1) Tiles that can be placed next to each other to recreate a larger image.
+ *    This is typical for tiles produced by cutting up a rastermap; each row
+ *    and column from the original map is included in exactly one tile and
+ *    never duplicated.
+ *    This is also encountered with OSM tiles, as they are designed to fit
+ *    together seamlessly to make the tile borders vanish.
+ * 2) Tiles that overlap by one pixel with the adjacent tiles.
+ *    This is common with tiles without projection ("geographic" projection /
+ *    "+proj=latlong") with a defined resolution (degree/pixel), such as
+ *    SRTM and other global DHM datasets.
+ *    For example, SRTM3 tiles span 1° and contain 1201 pixels. The first pixel
+ *    lies directly at e.g. 15.0000°, the last pixel at exactly 16.0000°. This
+ *    results in each pixel representing exactly 3 arc seconds. Obviously, the
+ *    last pixel overlaps with the next tile spanning 16.0000° - 17.0000°.
+ *
+ * Consequently, type (1) tiles must be placed next to each other to recreate
+ * the original image, while type (2) tiles must be overlapped by one pixel.
+ * To do this, we pretend that each tile is actually one pixel smaller (we
+ * "clip" the right-most column and bottom-most row). This is handled via
+ * SubmapWidth()/SubmapHeight().
+ *
+ * So, for type (1) tiles, has_overlap_pixel is false and m_overlap_pixel == 0.
+ * So, for type (2) tiles, has_overlap_pixel is true and m_overlap_pixel == 1.
+ */
+
 class EXPORT CompositeMap : public RasterMap {
     public:
         CompositeMap(unsigned int num_x, unsigned int num_y,
+                     bool has_overlap_pixel,
                      const std::vector<std::shared_ptr<RasterMap>> &submaps);
         CompositeMap(const std::wstring &fname_token);
         virtual GeoDrawable::DrawableType GetType() const;
@@ -36,15 +67,19 @@ class EXPORT CompositeMap : public RasterMap {
         }
         static std::vector<std::wstring>
         ParseFname(const std::wstring &fname,
-                   unsigned int *num_maps_x, unsigned int *num_maps_y);
+                   unsigned int *num_maps_x, unsigned int *num_maps_y,
+                   bool *has_overlap_pixel);
         static std::vector<std::shared_ptr<RasterMap>>
         LoadFnameMaps(const std::wstring &fname,
-                      unsigned int *num_maps_x, unsigned int *num_maps_y);
+                      unsigned int *num_maps_x, unsigned int *num_maps_y,
+                      bool *has_overlap_pixel);
         static std::wstring
         FormatFname(unsigned int num_maps_x, unsigned int num_maps_y,
+                    bool has_overlap_pixel,
                     const std::vector<std::wstring> &fnames);
         static std::wstring
         FormatFname(unsigned int num_maps_x, unsigned int num_maps_y,
+                    bool has_overlap_pixel,
                     const std::vector<std::shared_ptr<RasterMap>> &orig_maps);
         static std::wstring
         FormatFname(const class CompositeMap &this_map);
@@ -57,7 +92,11 @@ class EXPORT CompositeMap : public RasterMap {
         std::tuple<unsigned int, unsigned int, T>
         find_submap(T coord) const;
 
+        unsigned int SubmapWidth(unsigned int mx, unsigned int my) const;
+        unsigned int SubmapHeight(unsigned int mx, unsigned int my) const;
+
         unsigned int m_num_x, m_num_y;
+        int m_overlap_pixel;
 
         std::vector<std::shared_ptr<RasterMap> > m_submaps;
         unsigned int m_width, m_height;
