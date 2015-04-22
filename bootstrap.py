ce400ce4f1cdee42b14e7bed58a5162c045d7848
Christian Aichinger <Greek0@gmx.net>
2015-04-22 21:55:32 +0200

    Refactor bootstrap.py
    We don't need a full-blown copytree implementation, it's just a few
    files without subdirectories.

--- bootstrap.py
+++ bootstrap.py
@@ -8,16 +8,21 @@ import venv
 import shutil
 import subprocess
 
+def dircopy(src, dest):
+    os.makedirs(dest, exist_ok=True)
+    for file in os.listdir(src):
+        shutil.copy(os.path.join(src, file), dest)
+
 def make_venv(venv_dir):
     venv.create(venv_dir)
 
     python_include_dir = os.path.join(sys.prefix, 'include')
     venv_include_dir = os.path.join(venv_dir, 'include')
-    copytree(python_include_dir, venv_include_dir)
+    dircopy(python_include_dir, venv_include_dir)
 
     python_libs_dir = os.path.join(sys.prefix, 'libs')
     venv_libs_dir = os.path.join(venv_dir, 'libs')
-    copytree(python_libs_dir, venv_libs_dir)
+    dircopy(python_libs_dir, venv_libs_dir)
 
     # Enable building extensions with debug flags against a non-debug Python.
     # https://stackoverflow.com/questions/1236060
@@ -28,38 +33,6 @@ def make_venv(venv_dir):
         f.seek(0)
         f.truncate()
         f.write(content)
-
-def copytree(src, dst, symlinks=False):
-    names = os.listdir(src)
-    if not os.path.isdir(dst):
-        os.makedirs(dst)
-    errors = []
-    for name in names:
-        srcname = os.path.join(src, name)
-        dstname = os.path.join(dst, name)
-        try:
-            if symlinks and os.path.islink(srcname):
-                linkto = os.readlink(srcname)
-                os.symlink(linkto, dstname)
-            elif os.path.isdir(srcname):
-                copytree(srcname, dstname, symlinks)
-            else:
-                shutil.copy2(srcname, dstname)
-            # XXX What about devices, sockets etc.?
-        except OSError as why:
-            errors.append((srcname, dstname, str(why)))
-        # catch the Error from the recursive copytree so that we can
-        # continue with other files
-        except shutil.Error as err:
-            errors.extend(err.args[0])
-    try:
-        shutil.copystat(src, dst)
-    except OSError as why:
-        # can't copy file access times on Windows
-        if why.winerror is None:
-            errors.extend((src, dst, str(why)))
-    if errors:
-        raise Error(errors)
 
 def main():
     venv_dir = 'venv'
