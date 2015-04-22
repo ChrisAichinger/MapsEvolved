b3c49eba557a6fd7931cf4b1715a3749f315fbf7
Christian Aichinger <Greek0@gmx.net>
2015-04-22 13:18:42 +0200

    Move startup code into the mapsevolved package
    Make the mapsevolved package more self-contained by moving the startup
    code (wx.App initialization) from /MapsEvolved.py into mapsevolved.main.
    
    MapsEvolved.py remains behind as a thing wrapper.

--- MapsEvolved.py
+++ MapsEvolved.py
@@ -1,30 +1,6 @@
 #!venv/Scripts/python
-#-------------------------------------------------------------------------------
-#   Maps Evolved
-#   Copyright 2013-2014, Christian Aichinger <Greek0@gmx.net>
-#-------------------------------------------------------------------------------
 
-import sys
-import wx
-
-from mapsevolved import frmMain
-
-
-def main():
-    # Do not redirect errors to popup window.
-    app = wx.App(redirect=False)
-
-    # TODO: Make Windows not fiddle with our toolbar buttons.
-    # Need to rework our images to have proper alpha channels before, though.
-    # Cf. http://wxpython.org/Phoenix/docs/html/ToolBar.html
-    #if wx.GetApp().GetComCtl32Version() >= 600 and wx.DisplayDepth() >= 32:
-    #    wx.SystemOptions.SetOption("msw.remap", 2)
-
-    top = frmMain.MainFrame()
-    app.SetTopWindow(top)
-    top.Show()
-    app.MainLoop()
-
+from mapsevolved.main import main
 
 if __name__ == '__main__':
     main()
---
+++ mapsevolved/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#-------------------------------------------------------------------------------
+#   Maps Evolved
+#   Copyright 2013-2015, Christian Aichinger <Greek0@gmx.net>
+#-------------------------------------------------------------------------------
+
+import sys
+import wx
+
+from mapsevolved import frmMain
+
+
+def main():
+    # Do not redirect errors to popup window.
+    app = wx.App(redirect=False)
+
+    # TODO: Make Windows not fiddle with our toolbar buttons.
+    # Need to rework our images to have proper alpha channels before, though.
+    # Cf. http://wxpython.org/Phoenix/docs/html/ToolBar.html
+    #if wx.GetApp().GetComCtl32Version() >= 600 and wx.DisplayDepth() >= 32:
+    #    wx.SystemOptions.SetOption("msw.remap", 2)
+
+    top = frmMain.MainFrame()
+    app.SetTopWindow(top)
+    top.Show()
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
